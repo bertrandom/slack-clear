87e083bca247bfc8c707a97f403f9f3d9493308a
Bertrand Fan <bertrand@fan.net>
2018-03-16 01:18:17 -0700

    Initial import

---
+++ app.js
@@ -0,0 +1,89 @@
+const config = require('config');
+const express = require('express');
+const bodyParser = require('body-parser');
+const exphbs = require('express-handlebars');
+
+const rp = require('request-promise-native');
+
+const OAuth = require('oauth');
+const OAuth2 = OAuth.OAuth2;    
+const oauth2 = new OAuth2(config.slack.client_id,
+	config.slack.client_secret,
+	'https://slack.com/', 
+	'/oauth/authorize',
+	'/api/oauth.token', 
+	null);
+
+const { WebClient } = require('@slack/client');
+
+const app = express();
+
+app.engine('hb', exphbs({
+	defaultLayout: 'main',
+	extname: 'hb'
+}));
+
+app.set('view engine', 'hb');
+app.enable('view cache');
+
+app.use(express.static('static'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', function (req, res) {
+	res.render('home');
+});
+
+app.get('/install', function (req, res) {
+	res.redirect('https://slack.com/oauth/authorize?client_id=' + config.slack.client_id + '&scope=commands');
+});
+
+app.post('/command', function (req, res) {
+
+	if (req.body.token != config.slack.verification_token){
+		return res.sendStatus(401);
+	}
+
+	const responseUrl = req.body.response_url;
+
+	res.status(200).send();
+
+	rp({
+		uri: responseUrl,
+		json: true,
+		method: 'POST',
+		body: {
+			"text": " \n".repeat(50),
+			"response_type": "in_channel",
+		}
+	}).catch(function(e) {
+		console.log(e);
+	});
+
+});
+
+app.get('/oauth', function (req, res) {
+
+	oauth2.getOAuthAccessToken(
+		req.query.code,
+		{'grant_type':'client_credentials'},
+		function (e, access_token, refresh_token, results) {
+
+			if (e) {
+				return res.sendStatus(500);
+			}
+
+			res.redirect('https://slack.com/app_redirect?app=' + results.app_id);
+
+			let web = new WebClient(results.access_token);
+			web.chat.postMessage({ channel: results.authorizing_user_id, text: 'Thanks for installing Clear. You can clear the screen by typing `/clear`, try it now.' });
+
+		}
+	);
+
+});
+
+app.listen(config.port, function () {
+	console.log('Server started on port ' + config.port + '.');
+});
---
+++ views/layouts/main.hb
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Clear</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+	<meta name="slack-app-id" content="A9R31SQJH">
+</head>
+<body>
+	{{{body}}}
+</body>
+</html>
